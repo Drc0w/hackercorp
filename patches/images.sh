baab83a64b250c987dfab35b6686153559f693d6
Paul-Henry Perrissel <perris_p@epita.fr>
2017-01-24 22:05:12 +0100

    Remove old comments in script.
    Remove some old comments in script patches/images.sh

--- patches/images.sh
+++ patches/images.sh
@@ -3,10 +3,3 @@ for IMAGE in $(find images); do
   cp $IMAGE output/images/
 done
 echo Applying images in english version
-#cd ../output/
-#for i in $(find ./*/ | grep ".html"); do
-#	if [ ! -d $i ]; then
-#		sed -i s/src=\"\.\.\\/images/src=\"\.\.\\/\.\.\\/images/g $i
-#		sed -i s/src=\"\.\\/images/src=\"\.\.\\/images/g $i
-#	fi
-#done
